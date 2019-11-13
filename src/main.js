b056ab6f18ea5e7d0d9f1baf7c968ab64c6f26dd
Euphausiacea <15888836366@163.com>
2019-11-13 08:52:28 +0800

    fix(修改Element的语言)

--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-
+import lang from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import locale from 'element-ui/lib/locale'
 import '@/styles/index.scss' // global css
 
 import App from './App'
 import router from './router'
 
 import '@/icons' // icon
-
-Vue.use(ElementUI, { locale })
+locale.use(lang)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
