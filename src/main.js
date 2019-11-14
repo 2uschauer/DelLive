7df9d6835e97e7f7fae1e3f52f5c1cd5ad11ec76
Euphausiacea <ghoulsteven@gmail.com>
2019-11-14 20:02:21 +0800

    feature(增加了权限控制功能)

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import router from './router'
 import store from './store'
+import '@/routerControl'
 
 import '@/icons' // icon
 locale.use(lang)
---
+++ src/routerControl.js
@@ -0,0 +1,39 @@
+import router from '@/router'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress'
+import store from './store'
+store.dispatch('getUserInfo')
+
+const { routes } = store.getters
+const whilteList = ['/login']
+console.log(store.getters.routesStatus)
+if (routes && routes.length > 0) {
+  router.setRoutes(routes,router)
+}
+
+function checkPermission() {
+  const { userName, token, routes } = store.getters
+  return (userName && token && routes && routes.length > 0)
+}
+
+router.beforeEach((to, from ,next) => {
+  const { routes } = router.options
+  Nprogress.start()
+  if (checkPermission()) {
+    if (to.path === '/' && routes.length > 2) {
+      const route = routes[1]
+      next({ path: route.path })
+      return
+    }
+    next()
+  } else {
+    if (whilteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next({ path: '/login' })
+    }
+  }
+})
+router.afterEach((to, from ,next) => {
+  Nprogress.done()
+})
