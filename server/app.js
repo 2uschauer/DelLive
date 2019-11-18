8ebe4d89fcc728748251df318757db7a374d1c5a
Euphausiacea <ghoulsteven@gmail.com>
2019-11-18 16:43:28 +0800

    feature(新增node端直播功能)

--- server/app.js
+++ server/app.js
@@ -7,7 +7,23 @@ const returnJson = require('./utils/returnJson')
 const logger = require('./utils/log')
 const TagPlatForm = logger.TagPlatForm
 const app = express()
+const spawn = require('child_process').spawn;
+const subProcess = spawn('bash');
+function onData(data) {
+  process.stdout.write(data);
+}
 app.use('/backend', require('./utils/proxy')(config.ziker.appIntranetPrefix))
+const liveServerPath = path.join(__dirname,'/utils/live')
+subProcess.on('error', function () {
+  TagPlatForm.error('Error error is: ', arguments)
+});
+// 设置消息监听
+subProcess.stdout.on('data', onData);
+subProcess.stderr.on('data', onData);
+subProcess.on('close', (code) => { TagPlatForm.info(`Exit process code：${code}`) }); // 监听进程退出
+// 向子进程发送命令
+subProcess.stdin.write(`cd ${liveServerPath} \n`);
+subProcess.stdin.write('./livego \n');
 
 if (config.env !== 'env') {
   app.route('/*')
