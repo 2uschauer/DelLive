8bdcbed31afa30de2fa5f4ec569fc5f73a5fdf65
Euphausiacea <ghoulsteven@gmail.com>
2019-11-19 17:49:18 +0800

    fix(修改端口为80)

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   ip: process.env.IP || '0.0.0.0',
-  port: 9500,
+  port: 80,
   logsPath: '/Users/delpro/WorkSpace/DelLive/logs',
   ziker: {
     appIntranetPrefix: 'http://0.0.0.0'
