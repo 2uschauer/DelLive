9a44364034fc818a716b0ec9c3370a356ef62231
Euphausiacea <ghoulsteven@gmail.com>
2019-11-21 13:41:51 +0800

    fix(修改https端口)

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   ip: process.env.IP || '0.0.0.0',
-  port: 80,
+  port: 443,
   logsPath: '/Users/delpro/WorkSpace/DelLive/logs',
   ziker: {
     appIntranetPrefix: 'http://0.0.0.0'
