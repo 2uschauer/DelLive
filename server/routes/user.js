201fcf8fe7a42203fbb9ef96ea3f268e1160f233
Euphausiacea <ghoulsteven@gmail.com>
2019-11-26 11:17:09 +0800

    fix(登录增加了查询验证数据库操作)

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,41 @@
 'use strict'
+const { connectMongo, insertOne, find } = require('../utils/mongo')
+const RESULT = require('../utils/returnJson').RESULT
+// connectMongo()
+//   .then((db) => {
+//     return insertOne(db, 'documents', { a: 6 })
+//   })
+//   .then((db) => {
+//     return find(db, 'documents', { a: 6 })
+//   })
+//   .then((res) => {
+//     console.log(res)
+//   })
 module.exports = function() {
   const express = require('express')
   const router = express.Router()
   router.use('/auth/token',function(req, res) {
-    res.json({
-      responseCode: '000000',
-      responseMsg: '登陆成功',
-      data: '111'
-    })
+    const params = req.body
+    console.log(params)
+    connectMongo()
+      .then((db) => {
+        return find(db, 'user', params)
+      })
+      .then((doc) => {
+        if (doc) {
+          res.json({
+            responseCode: '000000',
+            responseMsg: '登陆成功',
+            data: '111'
+          })
+        } else {
+          console.log(RESULT.SIGN_IN_USERNAME_OR_PASSWORD_FAIL)
+          res.json(RESULT.SIGN_IN_USERNAME_OR_PASSWORD_FAIL)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   })
   router.use('/auth/getRoutesByToken', (req, res) => {
     res.json({
