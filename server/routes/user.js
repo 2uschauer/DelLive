38228b9c1b3d7cdd8e3c41af1d7a90b5547dabf2
Euphausiacea <ghoulsteven@gmail.com>
2019-11-26 17:22:27 +0800

    feature(增加了登出操作了redis存储操作)

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,72 +1,118 @@
 'use strict'
-const { connectMongo, insertOne, find } = require('../utils/mongo')
-const RESULT = require('../utils/returnJson').RESULT
-// connectMongo()
-//   .then((db) => {
-//     return insertOne(db, 'documents', { a: 6 })
-//   })
-//   .then((db) => {
-//     return find(db, 'documents', { a: 6 })
-//   })
-//   .then((res) => {
-//     console.log(res)
-//   })
+require('promise.prototype.finally').shim();
+const jwt = require('jwt-simple');
+const redis = require('redis')
+const { MongoClient } = require('mongodb');
+const { promisify } = require('util');
+const { TagPlatForm } = require('../utils/log')
+const { RESULT } = require('../utils/returnJson')
+const { find, insertOne, deleteOne } = require('../utils/mongo')
+const { encodePasseord } = require('../utils')
+const config = require('../config')
+const { mongo } = config
+const redisClient = redis.createClient(9882, '127.0.0.1')
+redisClient.on('error', (err) => { TagPlatForm.error(`${err}`) });
+const redisGet = promisify(redisClient.get).bind(redisClient);
+const redisSet = promisify(redisClient.set).bind(redisClient);
+const secret = 'DieLive';
+const url = `mongodb://${mongo.userName}:${mongo.password}@${mongo.url}:${mongo.port}/${mongo.db}`
 module.exports = function() {
   const express = require('express')
   const router = express.Router()
   router.use('/auth/token',function(req, res) {
     const params = req.body
-    console.log(params)
-    connectMongo()
-      .then((db) => {
-        return find(db, 'user', params)
-      })
-      .then((doc) => {
+    params.password = encodePasseord(params.password)
+    MongoClient.connect(url, function(err, mongoclient) {
+      if (err) TagPlatForm.error(`${err}`)
+      const db = mongoclient.db(mongo.db);
+      let token = null
+      find(db, 'user', params).then((doc) => {
         if (doc) {
+          token = jwt.encode(doc, secret , 'HS512')
+          return redisSet(token, doc.userName)
+        } else {
+          res.json(RESULT.SIGN_IN_USERNAME_OR_PASSWORD_FAIL)
+        }
+      }).then(() => {
+        res.json({
+          responseCode: '000000',
+          responseMsg: '登陆成功',
+          data: token
+        })
+      }).catch((err) => {
+        console.error(err)
+      }).finally(() => {
+        mongoclient.close()
+      })
+    })
+  })
+  router.use('/signUp', (req, res) => {
+    const params = req.body
+    params.password = encodePasseord(params.password)
+    let token = null
+    let inviteCode = null
+    MongoClient.connect(url, function(err, mongoclient) {
+      if (err) TagPlatForm.error(`${err}`)
+      const db = mongoclient.db(mongo.db);
+      find(db, 'InviteCode', { code: params.inviteCode }).then((doc) => {
+        inviteCode = doc
+        if (doc) {
+          return find(db, 'role', { role: doc.role })
+        } else {
+          res.json(RESULT.SIGN_IN_INVATE_CODE_EXPIRE)
+        }
+      }).then((doc) => {
+        return insertOne(db, 'user', {
+          userName: params.userName,
+          password: params.password,
+          email: params.email,
+          router: doc.router
+        }).then(() => {
+          token = jwt.encode(params.userName, secret , 'HS512')
+          return redisSet(token, params.userName)
+        }).then(() => {
+          return deleteOne(db, 'inviteCode', inviteCode)
+        }).then((doc) => {
           res.json({
             responseCode: '000000',
             responseMsg: '登陆成功',
-            data: '111'
+            data: token
           })
-        } else {
-          console.log(RESULT.SIGN_IN_USERNAME_OR_PASSWORD_FAIL)
-          res.json(RESULT.SIGN_IN_USERNAME_OR_PASSWORD_FAIL)
-        }
+        }).catch((err) => {
+          res.json({
+            responseCode: '000001',
+            responseMsg: err,
+            data: err
+          })
+        }).finally(() => {
+          mongoclient.close()
+        })
       })
-      .catch((err) => {
-        console.log(err)
-      })
+    })
   })
   router.use('/auth/getRoutesByToken', (req, res) => {
-    res.json({
-      responseCode: '000000',
-      responseMsg: '获取菜单列表成功',
-      data: [{
-        menuCode: 'live',
-        children: [{
-          menuCode: 'liveIndex',
-          children: null
-        },{
-          menuCode: 'liveList',
-          children: null
-        },{
-          menuCode: 'liveDetail',
-          children: null
-        }]
-      },{
-        menuCode: 'blog',
-        children: [{
-          menuCode: 'blogIndex',
-          children: null
-        },{
-          menuCode: 'blogList',
-          children: null
-        },{
-          menuCode: 'blogDetail',
-          children: null
-        }]
-      }]
-    })
+    const token = req.get('X-Authorization')
+    redisGet(token)
+      .then((userName) => {
+        if (userName) {
+          MongoClient.connect(url, function(err, mongoclient) {
+            if (err) TagPlatForm.error(`${err}`)
+            const db = mongoclient.db(mongo.db);
+            find(db, 'user', { userName: userName })
+              .then((doc) => {
+                if (doc) {
+                  res.json({
+                    responseCode: '000000',
+                    responseMsg: '获取菜单列表成功',
+                    data: doc.router
+                  })
+                } else {
+                  res.json(RESULT.UNAUTHORIZATION)
+                }
+              })
+          })
+        }
+      })
   })
   return router
 }
--- server/utils/mongo/index.js
+++ server/utils/mongo/index.js
@@ -1,21 +1,5 @@
 'use strict'
 const TagPlatForm = require('../log').TagPlatForm
-module.exports.connectMongo = function() {
-  const config = require('../../config')
-  const { mongo } = config
-  const MongoClient = require('mongodb').MongoClient;
-  const url = `mongodb://${mongo.userName}:${mongo.password}@${mongo.url}:${mongo.port}/${mongo.db}`
-  return new Promise((resolve,reject) => {
-    MongoClient.connect(url, function(err, client) {
-      if (err) {
-        TagPlatForm.error(`${err}`)
-        reject(err)
-      }
-      const db = client.db(mongo.db);
-      resolve(db)
-    })
-  })
-}
 
 module.exports.insertOne = function(db, name, doc) {
   const collection = db.collection(name);
@@ -25,13 +9,13 @@ module.exports.insertOne = function(db, name, doc) {
         TagPlatForm.error(`${err}`)
         reject(err)
       }
-      console.log('Inserted 1 document into the collection');
+      TagPlatForm.info(`Inserted ${JSON.stringify(doc)} document into the ${name} collection`);
       resolve(db)
     });
   })
 }
 
-module.exports.find = function(db, name,doc) {
+module.exports.find = function(db, name, doc) {
   const collection = db.collection(name);
   return new Promise((resolve,reject) => {
     collection.findOne(doc, function(err, docs) {
@@ -39,7 +23,21 @@ module.exports.find = function(db, name,doc) {
         TagPlatForm.error(`${err}`)
         reject(err)
       }
-      console.log('find the collection');
+      TagPlatForm.info(`find the ${JSON.stringify(doc)} in the ${name} collection`);
+      resolve(docs)
+    });
+  })
+}
+
+module.exports.deleteOne = function(db, name, doc) {
+  const collection = db.collection(name);
+  return new Promise((resolve,reject) => {
+    collection.deleteOne(doc, function(err, docs) {
+      if (err) {
+        TagPlatForm.error(`${err}`)
+        reject(err)
+      }
+      TagPlatForm.info(`Delete the ${JSON.stringify(doc)} in the ${name} collection`);
       resolve(docs)
     });
   })
