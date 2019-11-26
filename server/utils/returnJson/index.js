0b8218d1ccc49a36fe938dbcbe9e0b82a7cd082f
Euphausiacea <ghoulsteven@gmail.com>
2019-11-26 11:16:10 +0800

    fix(请求结果枚举字段修改)

--- server/utils/returnJson/index.js
+++ server/utils/returnJson/index.js
@@ -1,22 +1,22 @@
 'use strict'
 
 const RESULT = {
-  SUCCESS: { resultCode: '000000', resultMsg: '请求处理成功' },
-  SYSTEM_FAIL: { resultCode: '-100', resultMsg: '系统异常' },
-  ARGUMENT_NOT_VALID: { resultCode: '010000', resultMsg: '请求参数异常' },
-  SESSION_EXPRIE: { resultCode: '650113', resultMsg: 'session已过期' },
-  SIGN_IN_FAIL: { resultCode: '930101', resultMsg: '用户登录失败' },
-  SIGN_IN_USERNAME_OR_PASSWORD_FAIL: { resultCode: '930102', resultMsg: '用户名或密码错误' },
-  SIGN_IN_VERIFY_CODE_EXPIRE: { resultCode: '930103', resultMsg: '验证码已过期' },
-  SIGN_IN_LOCKED: { resultCode: '930107', resultMsg: '用户账号已锁定，请联系管理员解锁' },
-  SIGNATURE_NOT_VAILD: { resultCode: '930201', resultMsg: '签名验证不通过' },
-  REQUEST_NOT_VALID: { resultCode: '930202', resultMsg: '请求不符合规范，请确认后再提交' },
-  UNAUTHORIZATION: { resultCode: '930401', resultMsg: '访问无权限' },
+  SUCCESS: { responseCode: '000000', responseMsg: '请求处理成功' },
+  SYSTEM_FAIL: { responseCode: '-100', responseMsg: '系统异常' },
+  ARGUMENT_NOT_VALID: { responseCode: '010000', responseMsg: '请求参数异常' },
+  SESSION_EXPRIE: { responseCode: '650113', responseMsg: 'session已过期' },
+  SIGN_IN_FAIL: { responseCode: '930101', responseMsg: '用户登录失败' },
+  SIGN_IN_USERNAME_OR_PASSWORD_FAIL: { responseCode: '930102', responseMsg: '用户名或密码错误' },
+  SIGN_IN_VERIFY_CODE_EXPIRE: { responseCode: '930103', responseMsg: '验证码已过期' },
+  SIGN_IN_LOCKED: { responseCode: '930107', responseMsg: '用户账号已锁定，请联系管理员解锁' },
+  SIGNATURE_NOT_VAILD: { responseCode: '930201', responseMsg: '签名验证不通过' },
+  REQUEST_NOT_VALID: { responseCode: '930202', responseMsg: '请求不符合规范，请确认后再提交' },
+  UNAUTHORIZATION: { responseCode: '930401', responseMsg: '访问无权限' },
 }
 
 function ReturnJson(msg, data) {
-  this.resultCode = msg.resultCode || '000001'
-  this.resultMsg = msg.resultMsg || '请求处理失败'
+  this.responseCode = msg.responseCode || '000001'
+  this.responseMsg = msg.responseMsg || '请求处理失败'
   this.data = data || null
 }
 
@@ -29,7 +29,7 @@ function success(data) {
 }
 
 function NodeError(Msg, data) {
-  return new ReturnJson({ resultCode: '199999',resultMsg: Msg }, data)
+  return new ReturnJson({ responseCode: '199999',responseMsg: Msg }, data)
 }
 
 exports.RESULT = RESULT
@@ -37,3 +37,5 @@ exports.ReturnJson = ReturnJson
 exports.fail = fail
 exports.success = success
 exports.NodeError = NodeError
+
+// db.createUser({ user: 'live', pwd: 'v7f5WQHqkw', roles: [ { role: "readWrite", db: "live" } ] });
